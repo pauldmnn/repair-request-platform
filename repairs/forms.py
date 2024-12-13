a1917d315347a1972aaf7b7fa8f621b4273850f2
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-12-13 11:21:34 +0000

    Add form and views for repair requests

---
+++ repairs/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from.models import RepairRequest
+
+
+class RepairRequestForm(forms.ModelForm):
+    class Meta:
+        model = RepairRequest
+        fields = ['name', 'email', 'phone', 'location', 'category', 'description' ]
--- repairs/views.py
+++ repairs/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import RepairRequest
+from .forms import RepairRequestForm
 
 # Create your views here.
+def post_request(request):
+    if request.method == 'POST':
+        form = RepairRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('recent_requests')
+    else:
+        form = RepairRequestForm()
+    return render(request, 'repairs/post_request.html', {'form': form})
+
+def recent_requests(request):
+    requests = RepairRequest.objects.all().order_by('-created_at')
+    return render(request, 'repairs/recent_requests.html', {'requests': requests})
