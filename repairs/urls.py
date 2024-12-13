0029a09b083e0caa89dc388e7a43277604282302
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-12-13 14:09:36 +0000

    Fix templates and urls

--- repairs/urls.py
+++ repairs/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('post/', views.post_request, name='post_request'),
+    path('', views.post_request, name='post_request'),
     path('recent/', views.recent_requests, name='recent_requests'),
 ]
